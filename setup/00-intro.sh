c21d875aaa472aceec75039b32c2f09d7696f83f
Sharp <rachel.sharp@upbound.io>
2024-10-07 14:35:54 -0400

    Intro

--- setup/00-intro.sh
+++ setup/00-intro.sh
@@ -150,6 +150,7 @@ elif [[ "$HYPERSCALER" == "aws" ]]; then
     echo "[default]
 aws_access_key_id = $AWS_ACCESS_KEY_ID
 aws_secret_access_key = $AWS_SECRET_ACCESS_KEY
+aws_session_token = $AWS_SESSION_TOKEN
 " >aws-creds.conf
 
     kubectl --namespace crossplane-system \
